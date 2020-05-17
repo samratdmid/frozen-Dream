6ce6c4ece8bfbcd37a9291e22d3fe1df17c1db1c
Samrat Dutta <samratdmid721101@gmail.com>
2020-05-17 12:44:09 +0530

    1st correction in website

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -39,7 +39,7 @@ class Header extends Component{
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav}/>
-                        <NavbarBrand className="mr-auto" href="/">
+                        <NavbarBrand className="mr-auto" href="/home">
                             <img src={require("./assets/images/logo.png")} height="30" width="41" alt="Frozen Dream"/>
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
